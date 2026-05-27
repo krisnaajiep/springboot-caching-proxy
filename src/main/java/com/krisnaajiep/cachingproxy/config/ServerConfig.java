a904aff78c70b158e93986849b102ecc81757592
Krisna Ajie <krisnaajiep@gmail.com>
2026-05-28 00:58:08 +0700

    build(deps): add Spring Shell dependencies for interactive terminal application.
    - Enhance server origin argument handling.
    - Add clear cache command.
    - Implement integration tests for server config.

--- src/main/java/com/krisnaajiep/cachingproxy/config/ServerConfig.java
+++ src/main/java/com/krisnaajiep/cachingproxy/config/ServerConfig.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.shell.core.command.Command;
 
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Objects;
 
@@ -13,20 +16,37 @@ import java.util.Objects;
 @Configuration
 @RequiredArgsConstructor
 public class ServerConfig {
-    private final ServerProperties properties;
+    private final ServerProperties serverProperties;
+    private final RedisConnectionFactory redisConnectionFactory;
 
     @Bean
-    public ApplicationRunner run() {
+    public ApplicationRunner setServerOrigin() {
         return args -> {
-            if (args.containsOption("origin")) {
-                List<String> originList = args.getOptionValues("origin");
-
-                if (Objects.nonNull(originList) && !originList.isEmpty()) {
-                    properties.setOrigin(originList.getFirst());
-                }
+            if (!args.containsOption("server.origin")) {
+                return;
             }
 
-            log.info("Server origin is {}", properties.getOrigin());
+            List<String> optionValues = args.getOptionValues("server.origin");
+            if (Objects.isNull(optionValues) || optionValues.size() != 1 || optionValues.getFirst().isBlank()) {
+                throw new IllegalArgumentException("Invalid server origin value. Please provide a value for --server.origin");
+            }
+
+            serverProperties.setOrigin(optionValues.getFirst());
+            log.info("Server origin set to: {}", serverProperties.getOrigin());
         };
+    }
+
+    @Bean
+    public Command clearCache() {
+        return Command.builder()
+                .name("clear-cache")
+                .description("Clear the Redis cache")
+                .execute(context -> {
+                    try (PrintWriter printWriter = context.outputWriter()) {
+                        printWriter.println("Clearing all cache data...");
+                        redisConnectionFactory.getConnection().serverCommands().flushDb();
+                        printWriter.println("Cache cleared successfully.");
+                    }
+                });
     }
 }
--- src/test/java/com/krisnaajiep/cachingproxy/CachingProxyApplicationIT.java
+++ src/test/java/com/krisnaajiep/cachingproxy/CachingProxyApplicationIT.java
@@ -1,16 +1,65 @@
 package com.krisnaajiep.cachingproxy;
 
 import com.krisnaajiep.cachingproxy.config.ITConfig;
+import com.krisnaajiep.cachingproxy.config.ServerProperties;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.shell.test.ShellAssertions;
+import org.springframework.shell.test.ShellScreen;
+import org.springframework.shell.test.ShellTestClient;
+import org.springframework.shell.test.autoconfigure.ShellTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Import(ITConfig.class)
 class CachingProxyApplicationIT {
+    @Autowired
+    private ServerProperties serverProperties;
+
+    @Autowired
+    @Qualifier("setServerOrigin")
+    private ApplicationRunner serverOriginRunner;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void run_withEmptyServerOriginValues_shouldThrowIllegalArgumentException() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> serverOriginRunner.run(new DefaultApplicationArguments("--server.origin="))
+        );
+    }
+
+    @Test
+    void run_withServerOriginValue_shouldSetProperty() throws Exception {
+        String origin = "https://origintest.com";
+        serverOriginRunner.run(new DefaultApplicationArguments("--server.origin=" + origin));
+        assertEquals(origin, serverProperties.getOrigin());
+    }
+
+    @Nested
+    @ShellTest
+    class ShellCommandIT {
+        @Autowired
+        private ShellTestClient shellTestClient;
+
+        @Test
+        void execute_withClearCacheOption_shouldClearCache() throws Exception {
+            ShellScreen shellScreen = shellTestClient.sendCommand("clear-cache");
+
+            ShellAssertions.assertThat(shellScreen)
+                    .containsText("Clearing all cache data...")
+                    .containsText("Cache cleared successfully.");
+        }
     }
 
 }
